edb61311f860ece4c442f59e8fecf555dc118620
Eduardo Augusto Gomes de Oliveira <eduaugusto10@gmail.com>
2022-12-15 14:52:21 -0300

    copy to clipboard pix key

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ function App() {
               <span style={{ "fontSize": "10px", backgroundColor: "#fff" }}> {textCopy} </span>
             </button>
             <ul className='pix'>
-              <li>Digite esse número na descrição do seu PIX: 1234</li>
+              <li>Digite esse número na descrição do seu PIX: <span className='stronger'> 1234</span></li>
               <li>E assim conseguiremos confirmar o pagamento</li>
             </ul>
           </div>
