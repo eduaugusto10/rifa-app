5c0ead840563585027a78ea960712d6d3eb29429
Eduardo Augusto Gomes de Oliveira <eduaugusto10@gmail.com>
2022-12-15 15:31:01 -0300

    copy to clipboard pix key

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://18.216.236.25/api'
+    baseURL: 'http://18.220.168.76/api'
     // baseURL: 'http://localhost:5000'
 })
 
